6abda2493f04267dc9d2a82cae18c88e6cb8130e
Martin Pham <martin@Martins-MBP-2.lan>
2024-07-14 22:12:47 -0700

    started on navbar for landing page

--- application/client/src/pages/Landing.js
+++ application/client/src/pages/Landing.js
@@ -3,8 +3,14 @@ import "../styles/Landing.css"
 const Landing = () => {
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className="bg-white w-[30vw] h-[80vh] shadow-2xl rounded-lg flex flex-col">
-                
+            <div className="bg-white w-[30vw] h-[80vh] shadow-2xl rounded-lg flex flex-col p-[20px]">
+                {/* navbar to switch from sign in and sign up*/}
+                <div className="flex justify-center mt-4">
+                    <div className="w-[40%] bg-blue-500 text-white flex rounded-full overflow-hidden">
+                        <div>Sign In</div>
+                        <div>Sign Up</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
