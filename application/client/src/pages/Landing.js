62589932e0d127b8efb577d9f2b4ac405abcf686
Martin Pham <martin@Martins-MacBook-Pro-2.local>
2024-07-15 22:55:55 -0700

    added navbar for landing page form

--- application/client/src/pages/Landing.js
+++ application/client/src/pages/Landing.js
@@ -1,15 +1,35 @@
-import "../styles/Landing.css"
+import { useState } from 'react';
 
 const Landing = () => {
+    const [selected, setSelected] = useState('Sign In');
+
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="bg-white w-[30vw] h-[80vh] shadow-2xl rounded-lg flex flex-col p-[20px]">
-                {/* navbar to switch from sign in and sign up*/}
+                {/* Navbar to switch from sign in and sign up */}
                 <div className="flex justify-center mt-4">
-                    <div className="w-[40%] bg-blue-500 text-white flex rounded-full overflow-hidden">
-                        <div>Sign In</div>
-                        <div>Sign Up</div>
+                    <div className="relative w-[70%] bg-neutral text-white flex rounded-full overflow-hidden">
+                        <div
+                            className={`flex-1 py-2 text-center cursor-pointer ${selected === 'Sign In' ? 'bg-[#7A4DC4]' : 'hover:bg-secondary'}`}
+                            onClick={() => setSelected('Sign In')}
+                        >
+                            Sign In
+                        </div>
+                        <div className="absolute inset-0 m-auto w-10 h-10 bg-[#7A4DC4] rounded-full"></div>
+                        <div
+                            className={`flex-1 py-2 text-center cursor-pointer ${selected === 'Sign Up' ? 'bg-[#7A4DC4]' : 'hover:bg-secondary'}`}
+                            onClick={() => setSelected('Sign Up')}
+                        >
+                            Sign Up
+                        </div>
                     </div>
+                </div>
+                <div className="flex-1 mt-4">
+                    {selected === 'Sign In' ? (
+                        <div> {/* Sign In form goes here */}Sign In </div>
+                    ) : (
+                        <div> {/* Sign Up form goes here */}Sign Up </div>
+                    )}
                 </div>
             </div>
         </div>
